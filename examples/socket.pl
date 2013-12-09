d67633d16c6da7f6250091cfa1d894573f1f6f49
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2013-12-09 14:48:40 +0100

    Do not change default /etc/hosts location for examples

--- examples/socket.pl
+++ examples/socket.pl
@@ -6,17 +6,6 @@ use lib 'lib', '../lib';
 
 my $domain = $ARGV[0] || 'example.com';
 
-use File::Temp 'tempfile';
-
-if (not $ENV{PERL_ANYEVENT_HOSTS}) {
-    my ($fh, $filename) = tempfile(TMPDIR => 1);
-    say $fh '1.2.3.4 example.com';
-    say $fh '5.6.7.8 example.com';
-    say $fh 'fe00::1234 example.com';
-    close $fh;
-    $ENV{PERL_ANYEVENT_HOSTS} = $filename;
-}
-
 use AnyEvent::DNS::EtcHosts;
 
 my $guard = AnyEvent::DNS::EtcHosts->register;
