7290b5c72d93ad09929e9b8d7af0e2f8397819fc
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2014-07-14 19:44:32 +0200

    Bump year

--- lib/AnyEvent/DNS/EtcHosts.pm
+++ lib/AnyEvent/DNS/EtcHosts.pm
@@ -324,7 +324,7 @@ Marc Lehmann <schmorp@schmorp.de>
 
 =head1 LICENSE
 
-Copyright (c) 2013 Piotr Roszatycki <dexter@cpan.org>.
+Copyright (c) 2013-2014 Piotr Roszatycki <dexter@cpan.org>.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as perl itself.
